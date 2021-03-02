0a6692a8a127d7b9e6f280a474988db1557da993
will <will@invertedv.com>
2021-03-02 15:34:09 -0600

    packdage data bug fixed

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 
+# see https://www.remarkablyrestrained.com/python-setuptools-manifest-in/
 setup(
     name='mortgage_imports',
     version='1.0',
@@ -7,6 +8,7 @@ setup(
     url='',
     install_requires=['clickhouse-driver', 'numpy', 'pandas', 'setuptools_scm'],
     package_data={'mortgage_imports': ['sql/*']},
+    include_package_data = True,
     use_scm_version=True,
     license='MIT',
     author='Will Alexander',
