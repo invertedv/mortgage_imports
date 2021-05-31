a4aba58c20a6d613f15a2a8dc1ded6800e474b0c
will <will@invertedv.com>
2021-05-31 09:17:31 -0500

    WIP: adding freddie data

---
+++ mortgage_imports/freddie.py
@@ -0,0 +1,24 @@
+import pkg_resources
+import mortgage_imports.clickhouse_utilities as cu
+import os
+data_loc = '/mnt/driveb/freddie_data/'
+filename = 'historical_data1_time_Q12017.txt'
+#filename = 'tmp.txt'
+
+client = cu.make_connection()
+sql_loc = pkg_resources.resource_filename('mortgage_imports', 'sql/freddie') + '/'
+
+# add trailing / if needed
+if data_loc[-1] != "/": data_loc += "/"
+
+# create DB if not there
+cu.run_query("CREATE DATABASE IF NOT EXISTS freddie", client)
+
+#cu.run_query("DROP TABLE IF EXISTS freddie.raw_perf", client)
+#cu.run_query(sql_loc + "raw_perf_ct.sql", client, True)
+#cu.import_flat_file("freddie.raw_perf", data_loc + filename)
+filename = 'historical_data1_Q12017.txt'
+
+cu.run_query("DROP TABLE IF EXISTS freddie.raw_orig", client)
+cu.run_query(sql_loc + "raw_orig_ct.sql", client, True)
+cu.import_flat_file("freddie.raw_orig", data_loc + filename)
---
+++ mortgage_imports/sql/freddie/raw_perf_ct.sql
@@ -0,0 +1,38 @@
+/*
+
+ NIF - means Not In Fannie data
+ */
+CREATE TABLE freddie.raw_perf
+(
+    ln_id String,
+    month FixedString(6), /*YYYYMM*/
+    ln_c_prin Float32,
+    ln_dq_status_cd LowCardinality(FixedString(3)), /* Fannie is (2) */
+    ln_age Int16,
+    ln_rem_term_legal Int16,
+    ln_repurch_flg LowCardinality(FixedString(1)), /* NIF Y,N,<sp> */
+    ln_mod_flg LowCardinality(FixedString(1)),
+    ln_zb_cd LowCardinality(FixedString(2)), /* Fannie is (3) */
+    ln_zb_dt FixedString(6), /* YYYYMM, <blank> */
+    ln_c_ir Float32, /* Decimal or Percentage? */
+    ln_defrl_amt Float32, /* Current deferred amt */
+    ln_last_pay_dt FixedString(6),
+    fcl_ce_prcds Nullable(Float32), /* MI proceeds */
+    fcl_net_prcds Nullable(Float32), /* net sales proceeds */
+    fcl_reprch_mw_prcds Nullable(Float32), /* other proceeds e.g. make whole, hazard ins*/
+    fcl_cost Nullable(Float32),
+    fcl_recov_cost Nullable(Float32),
+    fcl_pres_cost Nullable(Float32),
+    fcl_taxes Nullable(Float32),
+    fcl_misc_cost Nullable(Float32),
+    fcl_loss Nullable(Float32), /* NIF, total loss Compare to fannie ln_t_credit_loss*/
+    mod_t_loss Nullable(Float32),
+    ln_stepmod_flg LowCardinality(FixedString(1)), /* NIF Y,N <sp>=not a step mod */
+    ln_dfrd_pay_flg LowCardinality(FixedString(1)), /* NIF Y,N Deferred Payment Plan */
+    ln_curr_eltv Nullable(Float32), /* NIF estimated current LTV from Freddie */
+    ln_zb_prin Nullable(Float32), /* NIF: UPB just prior to zero balance */
+    ln_dq_accr_int Nullable(Float32),
+    ln_dq_distr_flg LowCardinality(FixedString(1)), /* NIF DQ due to natural disaster Y,<other> */
+    borr_asst_plan LowCardinality(FixedString(1)))
+ENGINE = MergeTree()
+ORDER BY (ln_id, month)
