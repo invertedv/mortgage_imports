527b766434f3696bac32892d6710743fbe8a6e2f
will <will@invertedv.com>
2021-06-18 14:15:33 -0500

    moved creating map.zip3_st into clean_up.py

--- mortgage_imports/clean_up.py
+++ mortgage_imports/clean_up.py
@@ -2,8 +2,13 @@ import mortgage_imports.clickhouse_utilities as cu
 import pkg_resources
 
 def clean_up():
-    sql_loc = pkg_resources.resource_filename('mortgage_imports', 'sql/fhfa') + '/'
     client = cu.make_connection()
+    sql_loc = pkg_resources.resource_filename('mortgage_imports', 'sql/map') + '/'
+    cu.run_query("DROP TABLE IF EXISTS map.zip3_st", client)
+    cu.run_query(sql_loc + "zip3_st_ct.sql", client, True)
+    cu.run_query(sql_loc + "zip3_st_ins.sql", client, True)
+
+    sql_loc = pkg_resources.resource_filename('mortgage_imports', 'sql/fhfa') + '/'
     
     cu.run_query("DROP TABLE IF EXISTS fhfa.zip3_annotated", client)
     cu.run_query(sql_loc + "zip3_annotated_ct.sql", client, True)
